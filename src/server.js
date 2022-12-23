b48a53b9a55f316755714d4d781daf4224c3271b
RodrigoLuigi <digaor@gmail.com>
2022-12-23 10:08:35 -0300

    feat: create routes configs / create  UsersController

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 const migrationsRun = require('./database/sqlite/migrations');
 
+const routes = require('./routes');
 const express = require('express');
 
 migrationsRun();
 
 const app = express();
 app.use(express.json());
+
+app.use(routes);
 
 const PORT = 3333;
 app.listen(PORT, () =>
