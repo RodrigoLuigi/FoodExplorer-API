fe5cd43fb21fda9cb2d2a39c42bf9674c1aba7c3
RodrigoLuigi <digaor@gmail.com>
2022-12-29 23:37:41 -0300

    feat: add function index and filter by product name and ingredient

--- src/controllers/ProductsController.js
+++ src/controllers/ProductsController.js
@@ -32,7 +32,6 @@ class ProductsController {
     const { id } = request.params;
 
     const product = await knex('products').where({ id }).first();
-    /* const ingredients = await knex('product_ingredient').where({product_id: id}).orderBy('id'); */
 
     const ingredients = await knex('product_ingredient')
       .select([
@@ -56,10 +55,100 @@ class ProductsController {
   }
 
   async index(request, response) {
-    const resp = await knex('products');
+    const { name, ingredients } = request.query;
 
-    return response.json(resp);
+    let products;
+
+    if (ingredients) {
+      const filterIngredients = ingredients
+        .split(',')
+        .map((ingredient) => ingredient.trim());
+
+      products = await knex('ingredients')
+        .select([
+          'products.id',
+          'products.name',
+          'products.description',
+          'products.price',
+          'products.category_id',
+        ])
+        .whereLike('products.name', `%${name}%`)
+        .whereIn('ingredients.name', filterIngredients)
+        .innerJoin(
+          'product_ingredient',
+          'product_ingredient.ingredient_id',
+          'ingredients.id'
+        )
+        .innerJoin('products', 'products.id', 'product_ingredient.product_id')
+        .groupBy('products.name')
+        .orderBy('products.name');
+    } else {
+      products = await knex('products')
+        .whereLike('name', `%${name}%`)
+        .orderBy('id');
+    }
+
+    const productIngredients = await knex('product_ingredient')
+      .select([
+        'ingredients.id',
+        'ingredients.name',
+        'ingredients.imagePath',
+        'product_ingredient.product_id',
+      ])
+      .innerJoin(
+        'ingredients',
+        'ingredients.id',
+        'product_ingredient.ingredient_id'
+      );
+
+    const productWithIngredients = products.map((product) => {
+      const ingredientsProduct = productIngredients.filter(
+        (ingredient) => ingredient.product_id === product.id
+      );
+
+      return {
+        ...product,
+        ingredients: ingredientsProduct,
+      };
+    });
+
+    console.log(productIngredients);
+
+    return response.json(productWithIngredients);
   }
+
+  /* async index(request, response) {
+    const productsWithIngredients = await knex('products')
+      .select([
+        'products.id as productID',
+        'products.name as product_name',
+        knex.raw('GROUP_CONCAT (ingredients.name) as ingredients'),
+       /*  knex.raw(
+          'GROUP_CONCAT(json_object("ingredientID", ingredients.id, "ingredientName", ingredients.name)) as ingredients',
+        ),
+      ])
+      .innerJoin(
+        'product_ingredient',
+        'products.id',
+        'product_ingredient.product_id'
+      )
+      .innerJoin(
+        'ingredients',
+        'ingredients.id',
+        'product_ingredient.ingredient_id'
+      )
+      .groupBy("product_name")
+      .orderBy("products.id")
+      .then(response => {
+        /* response.forEach(product => {
+          const ingredients = product.ingredients.split(",");
+          product.ingredients = ingredients;
+        });
+        return response
+      })
+
+    return response.json(productsWithIngredients);
+  }*/
 }
 
 module.exports = ProductsController;
