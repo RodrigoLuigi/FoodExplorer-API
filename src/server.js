b3840d1cc4385108e325f82a1a69f496e0d7ad73
RodrigoLuigi <digaor@gmail.com>
2022-12-31 13:16:37 -0300

    feat: create utils/AppError.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
-const migrationsRun = require('./database/sqlite/migrations');
+require('express-async-errors');
 
-const routes = require('./routes');
+const migrationsRun = require('./database/sqlite/migrations');
+const AppError = require('./utils/AppError');
+
 const express = require('express');
+const routes = require('./routes');
 
 migrationsRun();
 
@@ -9,6 +12,22 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+app.use((error, request, response, next) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    });
+  }
+
+  console.error(error);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error.',
+  });
+});
 
 const PORT = 3333;
 app.listen(PORT, () =>
